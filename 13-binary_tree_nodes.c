df56249858975a4a4e9737b1621f32173510baf8
DominusJP <6775@holbertonstudents.com>
2023-10-02 15:04:30 -0700

    13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,25 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_leaves - Counts the leaves in a binary tree.
- * @tree: A pointer to the root node of the tree to count the leaves.
+ * binary_tree_nodes - Counts the nodes with at least 1 child in a binary tree.
+ * @tree: A pointer to the root node of the tree to count the nodes.
  *
- * Return: The number of leaves in the binary tree. If tree is NULL, return 0.
+ * Return: The number of non-leaf nodes in the binary tree. If tree is NULL, return 0.
  */
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+    size_t left_nodes, right_nodes;
+
     if (tree == NULL)
         return (0); /* If tree is NULL, return 0 */
 
-    /* If the node is a leaf (has no children), return 1 */
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+    /* Check if the node is a non-leaf node (has at least one child) */
+    if (tree->left != NULL || tree->right != NULL)
+    {
+        /* Recursively count non-leaf nodes in the left and right subtrees */
+        left_nodes = binary_tree_nodes(tree->left);
+        right_nodes = binary_tree_nodes(tree->right);
 
-    /* Recursively count the leaves in the left and right subtrees */
-    size_t left_leaves = binary_tree_leaves(tree->left);
-    size_t right_leaves = binary_tree_leaves(tree->right);
+        /* Return the sum of non-leaf nodes in the subtrees plus 1 (current node) */
+        return (1 + left_nodes + right_nodes);
+    }
 
-    /* Return the sum of leaves in the left and right subtrees */
-    return (left_leaves + right_leaves);
+    return (0); /* Node is a leaf (no children) */
 }
 
